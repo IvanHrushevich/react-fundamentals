06b188c3f4b47bfb78ef72fc23096b8c49abac06
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-10-08 13:25:28 +0300

    implement PostItem component, add some posts, add basic styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import Counter from "./components/Counter";
+import { useState } from "react";
+import PostItem from "./components/PostItem";
+
+import "./styles/App.css";
 
 function App() {
-  return <Counter></Counter>;
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Ruby", body: "Description" },
+    { id: 3, title: "C#", body: "Description" },
+  ]);
+
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center" }}>Post List</h1>
+      {posts.map((post) => (
+        <PostItem post={post} key={post.id} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
