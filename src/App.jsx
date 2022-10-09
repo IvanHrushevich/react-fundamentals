34de842d66f0a3ab73bd143ec9cf79ee3c6d8034
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-10-09 10:14:28 +0300

    implement filter logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
 
 import "./styles/App.css";
 
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "Javascript", body: "Description" },
-    { id: 2, title: "Ruby", body: "Some escription" },
+    { id: 2, title: "Ruby", body: "Some description" },
     { id: 3, title: "C#", body: "Description" },
     { id: 4, title: "C++", body: "Dummy Description" },
   ]);
 
   const [selectedSort, setSelectedSort] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const sortedPosts = useMemo(() => {
+    return selectedSort
+      ? [...posts].sort((a, b) =>
+          a[selectedSort].localeCompare(b[selectedSort])
+        )
+      : posts;
+  }, [selectedSort, posts]);
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }, [searchQuery, sortedPosts]);
 
   const createPost = (post) => {
     setPosts([...posts, post]);
@@ -27,9 +43,6 @@ function App() {
   const sortPosts = (sort) => {
     setSelectedSort(sort);
 
-    const sortedPosts = [...posts].sort((a, b) =>
-      a[sort].localeCompare(b[sort])
-    );
     setPosts(sortedPosts);
   };
 
@@ -37,6 +50,11 @@ function App() {
     <div className="App">
       <PostForm create={createPost} />
       <hr style={{ margin: "15px 0" }} />
+      <MyInput
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Search..."
+      />
       <MySelect
         options={[
           { value: "title", name: "By name" },
@@ -46,7 +64,11 @@ function App() {
         onChange={sortPosts}
         defaultValue="Sort by"
       />
-      <PostList posts={posts} title="Post List" remove={removePost} />
+      <PostList
+        posts={sortedAndSearchedPosts}
+        title="Post List"
+        remove={removePost}
+      />
     </div>
   );
 }
