c46d66661b34856141f697aaf70e793825201b96
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-10-09 16:54:52 +0300

    add animation with react-transition-group

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,3 +1,5 @@
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
 import PostItem from "./PostItem";
 
 const PostList = ({ posts, title, remove }) => {
@@ -5,14 +7,13 @@ const PostList = ({ posts, title, remove }) => {
     <div>
       <h1 style={{ textAlign: "center" }}>{title}</h1>
       {posts.length ? (
-        posts.map((post, index) => (
-          <PostItem
-            post={post}
-            key={post.id}
-            number={index + 1}
-            remove={remove}
-          />
-        ))
+        <TransitionGroup>
+          {posts.map((post, index) => (
+            <CSSTransition key={post.id} timeout={500} classNames="post">
+              <PostItem post={post} number={index + 1} remove={remove} />
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
       ) : (
         <h1 style={{ textAlign: "center" }}>No Posts</h1>
       )}
