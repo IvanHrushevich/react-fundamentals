0ef68b629c8ab6eede37fba9470481b0a66bcc64
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-10-11 15:39:40 +0300

    implement dummy login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import { privateRoutes, publicRoutes } from "./router/router";
+import { AuthContext } from "./context/index";
 
 import "./styles/App.css";
 
 function App() {
-  const isAuth = false;
+  const [isAuth, setIsAuth] = useState(false);
   const routes = isAuth ? privateRoutes : publicRoutes;
   const router = createBrowserRouter(routes);
 
-  return <RouterProvider router={router} />;
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuth,
+        setIsAuth,
+      }}
+    >
+      <RouterProvider router={router} />
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,27 @@
+import { useContext } from "react";
+
 import MyButton from "../components/UI/button/MyButton";
 import MyInput from "../components/UI/input/MyInput";
+import { AuthContext } from "../context/index";
 
 function Login() {
+  const { isAuth, setIsAuth } = useContext(AuthContext);
+  console.log("isAuth", isAuth);
+
   return (
     <div style={{ marginTop: 20 }}>
       <h2>Login page</h2>
       <form>
         <MyInput type="text" placeholder="login" />
         <MyInput type="password" placeholder="password" />
-        <MyButton type="button">Log in</MyButton>
+        <MyButton
+          type="button"
+          onClick={() => {
+            setIsAuth(true);
+          }}
+        >
+          Log in
+        </MyButton>
       </form>
     </div>
   );
