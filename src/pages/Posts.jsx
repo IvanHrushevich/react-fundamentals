6990796016f4c7647ad718776c3758b71c1a614a
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-10-11 22:32:29 +0300

    implement infinite scroll

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import PostFilter from "../components/PostFilter";
 import PostForm from "../components/PostForm";
@@ -13,8 +13,6 @@ import { usePosts } from "../hooks/usePosts";
 
 import { getPageCount } from "../utils/pages";
 
-import Pagination from "../components/UI/pagination/Pagination";
-
 function Posts() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
@@ -22,12 +20,15 @@ function Posts() {
   const [totalPages, setTotalPages] = useState(0);
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
+  const lastElement = useRef();
+  const observer = useRef();
 
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
   const [fetchPosts, isPostsLoading, postError] = useFetching(
     async (limit, page) => {
       const response = await PostService.getAll(limit, page);
-      setPosts(response.data);
+      const newPosts = response.data;
+      setPosts([...posts, ...newPosts]);
 
       const totalPostCount = response.headers["x-total-count"];
       const totalPages = getPageCount(totalPostCount, limit);
@@ -36,8 +37,23 @@ function Posts() {
   );
 
   useEffect(() => {
+    if (isPostsLoading) return;
+    if (observer.current) observer.current.disconnect();
+
+    const callback = (entries, observer) => {
+      if (entries[0].isIntersecting && page < totalPages) {
+        console.log(1);
+        setPage(page + 1);
+      }
+    };
+
+    observer.current = new IntersectionObserver(callback);
+    observer.current.observe(lastElement.current);
+  }, [isPostsLoading]);
+
+  useEffect(() => {
     fetchPosts(limit, page);
-  }, []);
+  }, [page]);
 
   const createPost = (post) => {
     setPosts([...posts, post]);
@@ -48,11 +64,6 @@ function Posts() {
     setPosts(posts.filter((post) => post.id !== id));
   };
 
-  const changePage = (page) => {
-    setPage(page);
-    fetchPosts(limit, page);
-  };
-
   return (
     <div className="container">
       <MyButton onClick={() => setModal(true)} style={{ marginTop: 30 }}>
@@ -61,27 +72,28 @@ function Posts() {
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
-
       <PostFilter filter={filter} setFilter={setFilter} />
+
       {postError && <h1>Error: {postError}</h1>}
-      {isPostsLoading ? (
+
+      {isPostsLoading && (
         <div
           style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
         >
           <Loader />
         </div>
-      ) : (
-        <PostList
-          posts={sortedAndSearchedPosts}
-          title="Post List"
-          remove={removePost}
-        />
       )}
-      <Pagination
-        totalPages={totalPages}
-        currentPage={page}
-        changePage={changePage}
+
+      <PostList
+        posts={sortedAndSearchedPosts}
+        title="Post List"
+        remove={removePost}
       />
+      <div
+        className="dummyElement"
+        style={{ height: 30 }}
+        ref={lastElement}
+      ></div>
     </div>
   );
 }
